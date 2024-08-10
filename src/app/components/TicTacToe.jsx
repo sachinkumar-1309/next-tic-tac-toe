6f851f51dee057302cfbe088e74c1b23c3ce6d37
Sachin kumar gupta <sachinkg1309@gmail.com>
2024-08-10 15:47:13 +0530

    updated UI

--- src/app/components/TicTacToe.jsx
+++ src/app/components/TicTacToe.jsx
@@ -7,20 +7,21 @@ const TicTacToe = () => {
 	const initialBoard = Array(9).fill(null);
 	const [board, setBoard] = useState(initialBoard);
 	const [currentPlayer, setCurrentPlayer] = useState("cross"); // Use "cross" and "circle"
+	const [winningCombination, setWinningCombination] = useState([]);
 	const [gameActive, setGameActive] = useState(true);
 
 	const handleCellClick = (index) => {
-		console.log(index);
 		if (board[index] || !gameActive) return;
 
 		const newBoard = [...board];
 		newBoard[index] = currentPlayer;
 		setBoard(newBoard);
 
-		const winner = checkForWinner(newBoard);
+		const { winner, combination } = checkForWinner(newBoard);
 		if (winner) {
 			setGameActive(false);
-			alert(`${winner === "cross" ? "X" : "O"} wins!`);
+			setWinningCombination(combination);
+			// alert(`${winner === "cross" ? "X" : "O"} wins!`);
 		} else if (!newBoard.includes(null)) {
 			setGameActive(false);
 			alert("Draw!");
@@ -44,30 +45,34 @@ const TicTacToe = () => {
 		for (let combination of winningCombinations) {
 			const [a, b, c] = combination;
 			if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-				return board[a];
+				return { winner: board[a], combination };
 			}
 		}
-		return null;
+		return { winner: null, combination: [] };
 	};
 
 	const resetGame = () => {
 		setBoard(initialBoard);
 		setCurrentPlayer("cross");
 		setGameActive(true);
+		setWinningCombination([]);
 	};
 
 	return (
 		<div className="relative flex justify-center items-center ">
 			<div className="">
 				<div className={styles.gameBoard}>
-					<div className="absolute sm:top-[-11%] top-[-15%] left-2 w-24 h-24">
+					<div className="absolute sm:top-[-11%] top-[-16%] left-2 sm:w-28 w-24 sm:h-28 h-24 ">
 						<div className={styles.cross}></div>
 					</div>
-					<div className="absolute sm:top-[-11%] top-[-15%] left-16 w-24 h-24">
+					<div className="absolute sm:top-[-11%] top-[-16%] left-16 sm:w-28 w-24 sm:h-28 h-24">
 						<div className={styles.circle}></div>
 					</div>
-					<div className="absolute sm:px-6 rounded-[8px] sm:top-[-13%] top-[-17%]  left-[38%] w-28 bg-[#1f3540] shadow-custom sm:h-12 h-11 text-[#a8bec9] font-extrabold sm:text-3xl text-2xl">
-						<div className="h-full w-full flex justify-center items-center text-[#a8bec9] ">{currentPlayer == "cross" ? "X" : "O"} <span className="ml-3 sm:text-xl text-lg  font-bold">TURN</span></div>
+					<div className="absolute sm:px-6 rounded-[8px] sm:top-[-13%] top-[-18%]  left-[38%] w-28 bg-[#1f3540] shadow-custom sm:h-12 h-12 text-[#a8bec9] font-extrabold sm:text-3xl text-2xl">
+						<div className="h-full w-full flex justify-center items-center text-[#a8bec9] ">
+							{currentPlayer == "cross" ? "X" : "O"}{" "}
+							<span className="ml-3 sm:text-xl text-lg  font-bold">TURN</span>
+						</div>
 					</div>
 
 					<button className={styles.resetButton} onClick={resetGame}>
@@ -76,10 +81,23 @@ const TicTacToe = () => {
 					{board.map((cell, index) => (
 						<div
 							key={index}
-							className={`${styles.cell} `}
+							className={`${
+								winningCombination.includes(index)
+									? styles.winningCell
+									: styles.cell
+							}`}
 							onClick={() => handleCellClick(index)}>
-							{cell === "cross" && <div className={styles.cross}></div>}
-							{cell === "circle" && <div className={styles.circle}></div>}
+							{cell === "cross" && <div className={`${
+								winningCombination.includes(index)
+									? styles.winningCross
+									: styles.cross
+							}`}></div>}
+							
+							{cell === "circle" && <div className={`${
+								winningCombination.includes(index)
+									? styles.winningCircle
+									: styles.circle
+							}`}></div>}
 						</div>
 					))}
 				</div>
