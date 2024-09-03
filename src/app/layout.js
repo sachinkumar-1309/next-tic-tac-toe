51cbe8c80ca4df73a1fe42e6f1fe6a9cab6ee293
Sachin kumar gupta <sachinkg1309@gmail.com>
2024-09-03 21:11:19 +0530

    added some customizations

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,22 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Head from 'next/head';
 
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Tic Tac Toe',
-  description: 'Developed by Sachin',
-}
+	title: "Tic Tac Toe",
+	description: "Developed by Sachin",
+};
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body >{children}</body>
-    </html>
-  )
+	return (
+		<html lang="en">
+        <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
+			<body>{children}</body>
+		</html>
+	);
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
-import TicTacToe from './components/TicTacToe';
+import TicTacToe from "./components/TicTacToe";
+
 
 export default function page() {
-  return (
-    <div className='w-screen flex justify-center items-center'>
-      {/* <h1 className='mb-4 text-blue-300'>Tic Tac Toe</h1> */}
-      <TicTacToe />
-    </div>
-  );
+	return (
+		<>
+			<div className="w-screen flex justify-center items-center">
+				{/* <h1 className='mb-4 text-blue-300'>Tic Tac Toe</h1> */}
+				<TicTacToe />
+			</div>
+		</>
+	);
 }
